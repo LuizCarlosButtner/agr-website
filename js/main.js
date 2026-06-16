740265dba4fdecffe9e7b47a79d6c0d4eac4ec93
Luiz Carlos Pereira <udroglot@gmail.com>
2026-06-16 14:58:32 -0300

    Ajuste de responsividade do menu lateral e desativação de cliques nas plaquinhas

--- js/main.js
+++ js/main.js
@@ -145,6 +145,11 @@ $(document).ready(function ($) {
 
   // Move to specific section when click on menu link
   link.on('click', function (e) {
+    // Ignora o clique se for na plaquinha de texto (span)
+    if (e.target.tagName.toLowerCase() === 'span') {
+      e.preventDefault();
+      return;
+    }
     var target = $($(this).attr('href'));
     $('html, body').animate({
       scrollTop: target.offset().top
